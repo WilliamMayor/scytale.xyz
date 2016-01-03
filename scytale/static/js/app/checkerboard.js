07765d76d8d99a0914485df334e504947f532de5
William Mayor <mail@williammayor.co.uk>
2016-01-03 20:57:06 +0000

    Update cipher pages with Helen's help

--- scytale/static/js/app/checkerboard.js
+++ scytale/static/js/app/checkerboard.js
@@ -2,13 +2,28 @@
     APP.checkerboard = function(selector) {
         var inputs = $(selector).find("table td input");
         var key = $(selector).find(".key pre");
-        var generate_key = function() {
+        var first_empty = $(selector).find(".empty.first");
+        var second_empty = $(selector).find(".empty.second");
+        var update = function() {
+            var blanks = 0;
             var text = inputs.map(function(i, e) {
-                return $(e).val();
+                var v = $(e).val();
+                if (v === "") v = " ";
+                if (i < 10) {
+                    if (v === " ") {
+                        if (blanks === 0) {
+                            first_empty.text(i);
+                            blanks++;
+                        } else {
+                            second_empty.text(i);
+                        }
+                    }
+                }
+                return v;
             }).get().join("");
             key.text(text);
         };
-        inputs.on("change input", generate_key);
-        generate_key();
+        inputs.on("change input", update);
+        update();
     };
 })(jQuery, APP || {});
