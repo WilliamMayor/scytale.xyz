752470d9b3c002486e036815f3dff167bca94620
William Mayor <mail@williammayor.co.uk>
2016-01-04 18:59:47 +0000

    Display key, plain and cipher texts better

--- scytale/jinja.py
+++ scytale/jinja.py
@@ -2,5 +2,10 @@ def ordinal(n):
     return str(n) + ("th" if 4 <= n % 100 <= 20 else {1: "st", 2: "nd", 3: "rd"}.get(n % 10, "th"))
 
 
+def display(text):
+    return text.replace(" ", "_")
+
+
 def init_app(app):
     app.jinja_env.filters["ordinal"] = ordinal
+    app.jinja_env.filters["display"] = display
