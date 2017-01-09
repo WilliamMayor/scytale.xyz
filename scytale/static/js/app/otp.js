a5e5a0fd155b406e1b79ea472247d4985e4f191d
William Mayor <mail@williammayor.co.uk>
2017-01-09 10:50:34 +0000

    Add one time pad page and calculator

---
+++ scytale/static/js/app/otp.js
@@ -0,0 +1,56 @@
+(function($, APP) {
+
+    function encrypt(plain, pad, alphabet) {
+        var cipher = $.map(plain.split(""), function(pl, i) {
+            var pa = pad[i % pad.length];
+            pl = alphabet.indexOf(pl);
+            pa = alphabet.indexOf(pa);
+            c = (pl + pa) % alphabet.length;
+            return alphabet[c];
+        });
+        return cipher.join("");
+    }
+
+    function decrypt(cipher, pad, alphabet) {
+        var plain = $.map(cipher.split(""), function(c, i) {
+            var pa = pad[i % pad.length];
+            c = alphabet.indexOf(c);
+            pa = alphabet.indexOf(pa);
+            // Need to add alphabet.length in order to force result to be
+            // positive.
+            pl = (c - pa + alphabet.length) % alphabet.length;
+            return alphabet[pl];
+        });
+        return plain.join("");
+    }
+
+    function clean(elm, alphabet) {
+        var val = elm.val().toUpperCase(),
+            not_alphabet = new RegExp("[^" + alphabet + "]", "g");
+        val = val.replace(not_alphabet, "");
+        elm.val(val);
+    }
+
+    APP.otp = function(alphabet) {
+        var plain = $("#plaintext");
+        var pad = $("#pad");
+        var cipher = $("#ciphertext");
+
+        plain.on("change input", function() {
+            clean(plain, alphabet);
+            cipher.val(encrypt(plain.val(), pad.val(), alphabet));
+        });
+
+        pad.on("change input", function() {
+            clean(pad, alphabet);
+            if (pad.val().length > 0) {
+                cipher.val(encrypt(plain.val(), pad.val(), alphabet));
+            }
+        });
+
+        cipher.on("change input", function() {
+            clean(cipher, alphabet);
+            plain.val(decrypt(cipher.val(), pad.val(), alphabet));
+        });
+    };
+})(jQuery, APP || {});
